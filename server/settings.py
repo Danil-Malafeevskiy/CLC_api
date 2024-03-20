d46e45e9b0e7df8938d77b22558a2877fcbe325f
danil1042 <dmalafeevskii@bk.ru>
2024-03-21 01:01:00 +0300

    fix CORS url

--- server/settings.py
+++ server/settings.py
@@ -1,6 +1,6 @@
 import os
 
-ORIGINS = os.environ.get("ORIGINS", "http://127.0.0.1:80,http://localhost:80").split(
+ORIGINS = os.environ.get("ORIGINS", "http://127.0.0.1:300,http://localhost:300").split(
     ","
 )
 
