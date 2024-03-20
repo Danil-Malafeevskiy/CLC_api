fea32b3b5fbac896c4d913e4adb5ce9e4791dd0e
danil1042 <dmalafeevskii@bk.ru>
2024-03-21 01:20:21 +0300

    fix CORS url

--- server/main.py
+++ server/main.py
@@ -6,13 +6,14 @@ from .api.endpoints.staff import router as staff_route
 from .api.endpoints.lesson import router as lesson_route
 from .api.endpoints.parent import router as parent_route
 from .api.endpoints.Child import router as child_route
+#from .api.endpoints.record import router as record_router
 
 
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=ORIGINS,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
@@ -22,3 +23,4 @@ app.include_router(staff_route)
 app.include_router(lesson_route)
 app.include_router(parent_route)
 app.include_router(child_route)
+#app.include_router(record_router)
